9a749aecdd8444080dacd70f2a3c98de842b0b6b
mormih <mormih@gmail.com>
2021-02-18 08:01:18 +0400

    l10n BottomAppBar instead of BottomNavigationBar FloatingButton added

---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,43 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "about_application" : MessageLookupByLibrary.simpleMessage("About application"),
+    "about_company" : MessageLookupByLibrary.simpleMessage("About company"),
+    "application_settings" : MessageLookupByLibrary.simpleMessage("Application Settings"),
+    "application_title_text" : MessageLookupByLibrary.simpleMessage("Application title"),
+    "build" : MessageLookupByLibrary.simpleMessage("Build"),
+    "company" : MessageLookupByLibrary.simpleMessage("Company"),
+    "contact_email" : MessageLookupByLibrary.simpleMessage("Contact e-mail"),
+    "copiedToClipboard" : MessageLookupByLibrary.simpleMessage("Copied to clipboard"),
+    "license_agreement" : MessageLookupByLibrary.simpleMessage("License agreement"),
+    "more_page" : MessageLookupByLibrary.simpleMessage("More"),
+    "phone" : MessageLookupByLibrary.simpleMessage("Phone"),
+    "platform" : MessageLookupByLibrary.simpleMessage("Platform"),
+    "privacy_policy" : MessageLookupByLibrary.simpleMessage("Privacy Policy"),
+    "servers" : MessageLookupByLibrary.simpleMessage("Servers"),
+    "thirdPartyLicenses" : MessageLookupByLibrary.simpleMessage("3rd party licenses"),
+    "version_name" : MessageLookupByLibrary.simpleMessage("Version"),
+    "website" : MessageLookupByLibrary.simpleMessage("Website"),
+    "whats_new" : MessageLookupByLibrary.simpleMessage("What\'s New")
+  };
+}
