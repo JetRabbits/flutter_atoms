ab83ec02acf525f857d9f720681b88a495b5e0d9
mormih <mormih@gmail.com>
2021-05-29 09:47:42 +0400

    fix pubspec

--- lib/flutter_atoms.dart
+++ lib/flutter_atoms.dart
@@ -1,19 +1,22 @@
 //@dart=2.9
 library flutter_atoms;
 
+import 'package:flutter/foundation.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+import 'package:logging/logging.dart';
+
+import 'flutter_atoms.config.dart';
+import 'navigation/models/navigation_model.dart';
+
+export 'package:logging/logging.dart';
 
 export 'generated/l10n.dart';
 export 'i18n/big_composite_message_lookup.dart';
 export 'models/models.dart';
 export 'navigation/navigation.dart';
 export 'widgets/widgets.dart';
-export 'package:logging/logging.dart';
 
-import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
-
-import 'flutter_atoms.config.dart';
-import 'navigation/models/navigation_model.dart';
 ///
 /// Service locator instance
 ///
@@ -25,4 +28,15 @@ import 'navigation/models/navigation_model.dart';
 void atomsSetup(NavigationModel navigationModel) {
   GetIt.I.registerSingleton<NavigationModel>(navigationModel);
   $initGetIt(GetIt.I);
+}
+
+void setupLogging({void Function(LogRecord) onRecord, Level forceLevel}) {
+  Logger.root.level = forceLevel ??
+      kDebugMode ? Level.ALL : Level.WARNING;
+
+  Logger.root.onRecord.listen(onRecord ??
+      (record) {
+        debugPrint(
+            '${record.time} [${record.level}] [${record.loggerName}] ${record.message}\n${record.stackTrace ?? ""}');
+      });
 }
