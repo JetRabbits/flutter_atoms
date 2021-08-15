bd0b2f2be61b0e3ad0b11de24ebab88b2d5e7070
mormih <mormih@gmail.com>
2021-08-15 15:52:15 +0400

    logging default file

--- lib/logging/send_bug_report.dart
+++ lib/logging/send_bug_report.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
 import 'package:archive/archive_io.dart';
-import 'package:share/share.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:share/share.dart';
 
-Future<void> sendLog(String errorLog) async {
+Future<void> sendLog({String errorLog: 'error.log'}) async {
   var directory = await getApplicationDocumentsDirectory();
   var _file = File("${directory.path}/$errorLog");
   // Zip a directory to out.zip using the zipDirectory convenience method
